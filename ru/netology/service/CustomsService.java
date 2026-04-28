2aa7256ff1347a965ffe41a499496cab77042df3
ZiCell <zicell@mail.ru>
2026-04-28 08:10:57 +0300

    Update CustomsService.java

--- ru/netology/service/CustomsService.java
+++ ru/netology/service/CustomsService.java
@@ -2,10 +2,10 @@ package ru.netology.service;
 
 public class CustomsService {
 
-        public static String finalPrice;
+        public static int finalPrice;
         
         public static int calcCustoms(int price, int weight) {
-        int finalPrice = price / 100 + weight * 100;
+        finalPrice = price / 100 + weight * 100;
         return finalPrice;
     }
 }
